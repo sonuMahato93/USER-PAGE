ea29dba1cbf8401490548f78212c61d431c00f94
sonu <mahatosonu177@gmail.com>
2022-05-31 14:00:39 +0530

    mongoose added

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const dotenv=require('dotenv');
 const morgan=require('morgan');
 const bodyparser=require('body-parser');
 const path=require("path");
-// const router=express.Router();
+const connectDB=require('./server/database/connection')
+
+// controller details
+const controller=require('./server/controller/controller')
+
+
+
+
+
 
 const app = express();
  
@@ -12,6 +20,9 @@ const PORT=process.env.PORT || 8080
 
 // log requests
 app.use(morgan('tiny'))
+
+// mongodb connection
+connectDB();
 
 
 // parse request to body prase
@@ -23,25 +34,22 @@ app.set('views engine', 'jade');
 app.engine('html', require('ejs').renderFile);
 app.set('views engine', 'html');
 
+// load views
 app.set("views",path.resolve(__dirname,"/views/index"))
 app.set("views",path.resolve(__dirname,"/views/update_user"))
 app.set("views",path.resolve(__dirname,"/views/add_user"))
 
 // load assests
+
 app.use('/css',express.static(path.resolve(__dirname,"assest/css")))
 app.use('/img',express.static(path.resolve(__dirname,"assest/img")))
 app.use('/js',express.static(path.resolve(__dirname,"assest/js")))
-// app.use('/index',express.static(path.resolve(__dirname,"views/index.html")))
-// app.use('/add_user',express.static(path.resolve(__dirname,"views/add_user.html")))
-// app.use('/update_user',express.static(path.resolve(__dirname,"views/update_user.html")))
 
 
-// app.get('/',function(req,res){
-//     res.sendFile(path.join(__dirname+'/views/index.html'));
-// })
+// routing
 
 app.get('/',function(req,res){
-    res.render(__dirname+'/views/index.html');
+    res.render(__dirname+'/views/index.html',{users:"New Data"});
 })
  app.get('/add-user',(req,res)=>{
     res.render(__dirname+'/views/add_user.html');
@@ -52,5 +60,25 @@ app.get('/update-user',(req,res)=>{
 })
 
 
+// API ROUTING
+app.post('/api/users',controller.create);
+app.get('/api/users',controller.find);
+app.put('/api/users/:id',controller.update);
+app.delete('/api/users/:id',controller.delete);
+ 
 
- app.listen(3000,()=>{console.log(`Server is running on https://localhost:${PORT}`)})
+
+
+// module.exports=route
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{console.log(`Server is running on https://localhost:${PORT}`)})
